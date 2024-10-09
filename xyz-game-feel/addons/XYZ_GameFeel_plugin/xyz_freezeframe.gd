3dae306b6191f52b088dcc1ad5bb29af626edb78
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-10-09 16:55:00 +0200

    completed example node + commented freezeframe vars

--- xyz-game-feel/addons/XYZ_GameFeel_plugin/xyz_freezeframe.gd
+++ xyz-game-feel/addons/XYZ_GameFeel_plugin/xyz_freezeframe.gd
@@ -1,15 +1,22 @@
 extends Node
 class_name XYZ_FreezeFrame
+## The freeze frame class manipulates time itself using the Engine.time_scale property.[br][br]
+## Use it for short freeze frames, or slow motion effects. 
+## Instantiate only one object of this class, as multiple objects might conflict
+## with each other.
+##
+## Class from the XYZ GameFeel Plugin.
 
-
+## When the freeze frame and potential queued freeze frames have been completed.
 signal freeze_frame_ended
 
-
-var default_time_scale := Engine.time_scale
 var remaining_freeze_frame := 0.0
 var in_freeze_frame := false
 
+## The default engine timescale
+@onready var default_time_scale := Engine.time_scale
 
+## Freeze the game for the duration
 func freeze_frame(duration : float = 0.015) -> void:
 	if in_freeze_frame:
 		remaining_freeze_frame += duration
@@ -27,6 +34,7 @@ func freeze_frame(duration : float = 0.015) -> void:
 	emit_signal("freeze_frame_ended")
 
 
+## Set the game to the desired time_scale for the duration
 func slow_motion(time_scale : float, duration : float) -> void:
 	if in_freeze_frame:
 		await self.freeze_frame_ended
@@ -35,6 +43,8 @@ func slow_motion(time_scale : float, duration : float) -> void:
 	Engine.time_scale = default_time_scale
 
 
+## Change the desired default time scale.
+## Default time scale affects the behaviors of the freeze_frame and slow_motion methods.
 func set_default_time_scale(time_scale : float) -> void:
 	Engine.time_scale = time_scale
 	default_time_scale = time_scale
