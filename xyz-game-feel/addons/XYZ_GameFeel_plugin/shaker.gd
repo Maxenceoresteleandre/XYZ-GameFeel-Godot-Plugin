8d78ae98176c1e49fab20bd95d70451685faa51f
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-10-09 12:34:12 +0200

    finished shaker class, to be tested

--- xyz-game-feel/addons/XYZ_GameFeel_plugin/shaker.gd
+++ xyz-game-feel/addons/XYZ_GameFeel_plugin/shaker.gd
@@ -1,56 +1,64 @@
 extends Node
-class_name Shaker
+class_name XYZ_Shaker
 ## The shaker class is meant to be used for camera shakes, object shakes, etc.
-##
 ## You may use one shaker for each individual object you want to shake, as trying 
 ## to trigger multiple shakes at the same time will fail.
+##
+## Class from the XYZ GameFeel Plugin.
 
-enum ShakableTypes {Float, Vector2, Vector3}
+
+## Emitted when object has finished shaking and can be called once more.
+signal shake_finished
+
+## Reset values if set_to_zero_after_shake is true.
 const ZERO_VALUES : Dictionary = {
-	ShakableTypes.Float : 0.0,
-	ShakableTypes.Vector2 : Vector2.ZERO,
-	ShakableTypes.Vector3 : Vector3.ZERO
+	TYPE_FLOAT : 0.0,
+	TYPE_COLOR : Color.WHITE,
+	TYPE_VECTOR2 : Vector2.ZERO,
+	TYPE_VECTOR3 : Vector3.ZERO
 }
-# OR:
-# float, vector2, vector3, color
-# and check property type
-# and if property is of correct type then shaaaaake it ! 
 
-
-## The object you want to shake
+## The object you want to shake.
 @export var object : Node
-# The property path of the affected property
+## The path of the affected property.
 @export var property := "offset"
-# The type of the property
-@export var property_type : ShakableTypes = ShakableTypes.Vector2
-@export var shake_trans := Tween.TRANS_SINE
-@export var shake_ease := Tween.EASE_IN_OUT
+## true: set the value to zero after the shake.
+## false: reset to the initial value after the shake.
 @export var set_to_zero_after_shake := true
+## The transition type used by the tween.
+@export var shake_trans := Tween.TRANS_SINE
+## The ease type used by the tween.
+@export var shake_ease := Tween.EASE_IN_OUT
 
 @onready var shake_timer : Timer = Timer.new()
 
-var initial_value : Variant = Vector2.ZERO
+var initial_value : Variant
+var reset_value : Variant
 var amplitude := 0
 var priority := 0
 var shaking := false
 var frequency := 0.0
+var property_type : int
 
-func _ready() -> void:
-	shake_timer.autostart = false
-	shake_timer.one_shot = true
-
+## Shake the object's property for duration_n, with jumps between -amplitude_n and amplitude_n,
+## at frequency_n. If another shake is happening, priority_n is used to choose whether or not
+## to override the previous shake with the new one.
 func shake(duration_n := 0.2, frequency_n := 15, amplitude_n := 30, priority_n := 0) -> void:
 	if shaking and priority_n < priority:
 		return
 	priority = priority_n
 	amplitude = amplitude_n
 	shaking = true
+	initial_value = object.get(property)
 	if set_to_zero_after_shake:
-		initial_value = ZERO_VALUES[property_type]
-		if typeof(object.get(property)):
-			pass
+		property_type = typeof(object.get(property))
+		if property_type in ZERO_VALUES.keys():
+			reset_value = ZERO_VALUES[property_type]
+		else:
+			print_debug("XYZ_Shaker: property type can't be shaken")
+			return
 	else:
-		initial_value = object.get(property)
+		reset_value = initial_value
 	frequency = 1/float(frequency_n)
 	_new_shake()
 	shake_timer.start(duration_n)
@@ -58,20 +66,56 @@ func shake(duration_n := 0.2, frequency_n := 15, amplitude_n := 30, priority_n :
 	shaking = false
 	_reset()
 
+func _ready() -> void:
+	shake_timer.autostart = false
+	shake_timer.one_shot = true
+
+func _get_random_float_offset(f_amplitude : float) -> float:
+	return randf_range(-f_amplitude, f_amplitude)
+
+func _get_random_color_offset(f_amplitude : float, transparency : float) -> Color:
+	return Color(
+		randf_range(-f_amplitude, f_amplitude), 
+		randf_range(-f_amplitude, f_amplitude), 
+		randf_range(-f_amplitude, f_amplitude), 
+		transparency
+	)
+
+func _get_random_vector2_offset(f_amplitude : float) -> Vector2:
+	return Vector2(
+		randf_range(-f_amplitude, f_amplitude),
+		randf_range(-f_amplitude, f_amplitude)
+	)
+
+func _get_random_vector3_offset(f_amplitude : float) -> Vector3:
+	return Vector3(
+		randf_range(-f_amplitude, f_amplitude),
+		randf_range(-f_amplitude, f_amplitude),
+		randf_range(-f_amplitude, f_amplitude)
+	)
+
 func _new_shake() -> void:
-	var rand := Vector2()
-	rand.x = randf_range(-amplitude, amplitude)
-	rand.y = randf_range(-amplitude, amplitude)
-	await _tween_shake(rand).finished
+	var offset_val : Variant
+	match property_type:
+		TYPE_FLOAT:
+			offset_val = _get_random_float_offset(amplitude)
+		TYPE_COLOR:
+			offset_val = _get_random_color_offset(amplitude, initial_value.a)
+		TYPE_VECTOR2:
+			offset_val = _get_random_vector2_offset(amplitude)
+		TYPE_VECTOR3:
+			offset_val = _get_random_vector3_offset(amplitude)
+	await _tween_shake(offset_val).finished
 	if shaking:
 		_new_shake()
 
-func _tween_shake(new_pos : Variant) -> Tween:
+func _tween_shake(offset_val : Variant) -> Tween:
 	var tween : Tween = create_tween().set_ease(shake_ease).set_trans(shake_trans)
-	tween.tween_property(object, property, initial_value + new_pos, frequency)
+	tween.tween_property(object, property, initial_value + offset_val, frequency)
 	return tween
 
 func _reset() -> void:
 	var tween : Tween = create_tween().set_ease(shake_ease).set_trans(shake_trans)
-	tween.tween_property(object, property, initial_value, frequency)
+	tween.tween_property(object, property, reset_value, frequency)
 	priority = 0
+	emit_signal("shake_finished")
